3825029dc15be58f7f40ffe4d408be6aca4bdcb7
Razvan Suprovici <razvan.suprovici@gmail.com>
2017-02-02 17:48:22 -0800

    styling and ux updates

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -68,12 +68,17 @@ export default class Game extends Component {
     .then((response) => response.json())
     .then((responseJson) => {
       this.setState({game: responseJson.game});
-      console.log(this.state);
+      this.clearInputs();
       return this.state;
     })
     .catch((error) => {
       console.error(error);
     });
+  }
+
+  clearInputs() {
+    this.refs.letter.value = '';
+    this.refs.word.value = '';
   }
 
   guessWord(word) {
@@ -94,6 +99,7 @@ export default class Game extends Component {
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({game: responseJson.game});
+        this.clearInputs();
         return this.state;
       })
       .catch((error) => {
@@ -105,44 +111,58 @@ export default class Game extends Component {
   }
 
   render() {
+    let contents = [];
     if(!this.state.game) {
-      return <button onClick={() => {this.newGame()}}>New Game</button>
+      contents = <div>
+        <button onClick={() => {this.newGame()}}>New Game</button>
+      </div>
     }
+    else if(this.state.game.mistakes > 6) {
+      const hangmanDead = require('./images/hangman-dead.jpg');
 
-    if(this.state.game.mistakes > 6) {
-      return <div>
-        <h1>You Lost :(</h1>
+      contents = <div>
+        <img src={hangmanDead} alt="hangman"/>
+        <h3>You Lose</h3>
         <button onClick={() => {this.newGame()}}>New Game</button>
       </div>
     } else if (this.state.game.progress.indexOf('_') === -1) {
-      return <div>
-        <h1>You Win :)</h1>
+      const hangmanDance = require('./images/hangman-dance.gif');
+
+      contents =  <div>
+
+        <img src={hangmanDance} alt="hangman"/>
+        <h3>You Win!</h3>
         <button onClick={() => {this.newGame()}}>New Game</button>
       </div>
-    }
-
-    return (
-      <div>
-        <h1>
-          Hangman
-        </h1>
-
+    } else {
+      contents = <div>
         <img src={this.imageList[this.state.game.mistakes]} alt="hangman"/>
         <h1>{this.state.game.progress}</h1>
 
         <div className='alert'>{this.state.alert}</div>
-        <div>
+        <div className='left'>
           <h1>
             <input ref="letter" size="1" onChange={() => {this.clearAlert()}}></input>
           </h1>
           <button onClick={() => this.guessLetter(this.refs.letter.value)}>Guess Letter</button>
 
         </div>
-        <div>
+        <div className='right'>
           <h1>
             <input ref="word" size={this.state.game.progress.length} onChange={() => {this.clearAlert()}}></input>
           </h1>
           <button onClick={() => this.guessWord(this.refs.word.value)} >Guess Word</button>
+        </div>
+      </div>
+    }
+
+    return (
+      <div className="page">
+        <div className="container">
+
+          <img className="logo" src={require('./images/hangman-logo.png')} alt="hangman"/>
+
+          {contents}
         </div>
       </div>
     );
